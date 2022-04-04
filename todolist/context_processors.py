96c51d15d54737a70d16eda336d782fea4c2418e
X00174641 Jason Fung <x00174641@mytudublin.ie>
2022-04-04 13:59:44 +0100

    Added pagination to todolist

--- todolist/context_processors.py
+++ todolist/context_processors.py
@@ -1,6 +1,7 @@
 from .models import ToDoTask
 from django.utils import timezone
 from datetime import timedelta
+from django.core.paginator import Paginator , EmptyPage, InvalidPage
 
 def todotasks(request):
     if request.user.is_authenticated:
@@ -9,6 +10,15 @@ def todotasks(request):
         for i in tasks:
             if timezone.now() + timedelta(minutes=60) >= i.due_date:
                 i.completed = True
-                i.save()    
-        return dict(tasks=tasks,taskcount=taskscount)
+                i.save()
+        paginator = Paginator(tasks, 6)    
+        try:
+            page = int(request.GET.get("page", "1"))
+        except:
+            page = 1
+        try:
+            tasks = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            tasks = paginator.page(paginator.num_pages)
+        return dict(tasks=tasks,taskcount=taskscount,page=page)
     return {}
